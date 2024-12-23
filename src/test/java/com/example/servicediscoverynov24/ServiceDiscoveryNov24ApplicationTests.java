6c06d2b7a183d2f8d7ecf6df5449792c58996697
apple <nikhiljain1203@gmail.com>
2024-12-23 22:55:11 +0530

    First Class

---
+++ src/test/java/com/example/servicediscoverynov24/ServiceDiscoveryNov24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servicediscoverynov24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryNov24ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
